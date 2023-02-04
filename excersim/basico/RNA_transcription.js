ebbe58f198ab97248c136b2c8d839e87db878623
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-02-03 21:02:50 -0600

    Se agrego el algoritmo RNA y renombre de del md

---
+++ excersim/basico/RNA_transcription.js
@@ -0,0 +1,26 @@
+function Rna_transcription(nucleotide1,nucleotide2,nucleotide3,nucleotide4){
+    let strand=[nucleotide1,nucleotide2,nucleotide3,nucleotide4];
+    let newStrand="";
+    for(let i=0;i<strand.length;i++){
+        console.log(strand[i]);
+        switch (strand[i]) {
+            case "a":
+                newStrand+="u";
+                break;
+            case "g":
+                newStrand+="c";
+                break;
+            case "c":
+                newStrand+="g";
+                break;
+            case "t":
+                newStrand+="a"
+                break;
+        
+        }
+    }
+    return newStrand;
+}
+
+let s1="a",s2="g",s3="c",s4="t";
+console.log("The RNA is "+Rna_transcription(s1,s2,s3,s4));
