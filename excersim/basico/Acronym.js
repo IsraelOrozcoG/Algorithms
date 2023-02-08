fe5c44149c1b2dbaf7b4858049bbc84e6d551961
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-02-07 19:38:52 -0600

    Se agrego algoritmo Acronym

---
+++ excersim/basico/Acronym.js
@@ -0,0 +1,16 @@
+function acronym(text){
+    let myAcronym="";
+    for(let i=0;i<text.length;i++){
+        if(i==0){
+            myAcronym+=text.charAt(i);
+        }
+        if(text.charAt(i)==" "){
+            myAcronym+=text.charAt(i+1);
+        }
+    }
+    return myAcronym.toUpperCase();
+}
+let phrase="This is pretty Awesome"
+let phrase2="What does that supposed to mean?"
+console.log(acronym(phrase));
+console.log(acronym(phrase2));
