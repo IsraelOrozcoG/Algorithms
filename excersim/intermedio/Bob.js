1e06becb41bf3e105e414f015b93e313af43b8e3
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-02-14 22:51:26 -0600

    Se agrego algoritmo Bob

--- excersim/intermedio/Bob.js
+++ excersim/intermedio/Bob.js
@@ -1,16 +1,18 @@
 let bob =(text) =>{
-
-      return text.charAt(text.length-1)== "?" ? "Sure"
-            : text.length==0 ? "Whoa, chill out!"
-            : text.length==1 ? "Calm down, I know what I'm doing!"
-            : text.length==2 ? "Fine. Be that way"
-            : "Whatever.";
+      let loudQuestion=/[A-Z" "]+\?{1}$/
+      let simpleQuestion=/[A-Za-z" "]+\?{1}$/
+      let yell=/[A-Z" "]+$/
+      return loudQuestion.test(text) ? `${text} -> Calm down, I know what I'm doing!`
+            : simpleQuestion.test(text) ? `${text} -> Sure`
+            : yell.test(text) ? `${text} -> Whoa, chill out!`
+            : text=="" ? `"${text}" -> Fine. Be that way`
+            : `${text} -> Whatever.`;
 }
 
 let phrase="IS THAT AMAZING?";
-let phrase1="";
-let phrase2="1";
-let phrase3="12";
+let phrase1="Is that amazing?";
+let phrase2="THIS IS AMAZING";
+let phrase3="";
 let phrase4= "You blah blah blah";
 console.log(bob(phrase));
 console.log(bob(phrase1));
