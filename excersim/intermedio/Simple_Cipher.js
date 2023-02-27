87604a9c8362b656874c6557aac14fcfc128dd25
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-02-26 20:06:19 -0600

    Primera parte del algoritmo Simple Cipher completada

--- excersim/intermedio/Simple_Cipher.js
+++ excersim/intermedio/Simple_Cipher.js
@@ -1,9 +1,27 @@
 const ALPHABET=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
-"P","Q","R","S","U","V","W","X","Y","Z"];
+"P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 let simpleCipher=(text)=>{
-    console.log(ALPHABET[0]);
-    console.log(ALPHABET.length);
+    text=text.toUpperCase();
+    let encrypted ="";
+    let position = [];
+    let helpVariable =0;
+    for(let i=0 ;i<text.length;i++){
+        console.log(text.charAt(i));
+       
+        position.push(ALPHABET.indexOf(text.charAt(i)));
+        console.log(position);
+    }
+    for(let j=0;j<text.length;j++){
+        helpVariable=position[j];
+        position[j]=helpVariable+2;
+    }
+    for(let i=0; i<text.length;i++){
+        //helpVariable=position[i];
+        //position[i]=helpVariable-2;
+    encrypted+=ALPHABET[position[i]];
+    }
+    console.log(`Well it seems->${encrypted}`)
     console.log(text);
 }
 let phrase="thereissomethinghere";
