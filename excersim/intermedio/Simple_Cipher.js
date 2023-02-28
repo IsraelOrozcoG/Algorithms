9c1bca6a28258fab4b6c04ebe1a76baef0cc7363
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-02-27 19:34:08 -0600

    Se agrego la funcionalidad de encriptar con cualquier valor del abecedario al algoritmo Simple Cipher

--- excersim/intermedio/Simple_Cipher.js
+++ excersim/intermedio/Simple_Cipher.js
@@ -1,28 +1,36 @@
-const ALPHABET=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
-"P","Q","R","S","T","U","V","W","X","Y","Z"];
+const ALPHABET=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-let simpleCipher=(text)=>{
-    text=text.toUpperCase();
+let simpleCipher=(text,letter)=>{
+    text=text.toLowerCase();
     let encrypted ="";
     let position = [];
-    let helpVariable =0;
+    let alphabetValue =0;
+    let letterValue = 0
+    letterValue=ALPHABET.indexOf(letter.charAt(0));
+    
     for(let i=0 ;i<text.length;i++){
-        console.log(text.charAt(i));
-       
+        //console.log(text.charAt(i));
         position.push(ALPHABET.indexOf(text.charAt(i)));
-        console.log(position);
+        //console.log(position);
     }
     for(let j=0;j<text.length;j++){
-        helpVariable=position[j];
-        position[j]=helpVariable+2;
+        alphabetValue=position[j];
+        if(letterValue+alphabetValue>26){
+            letterValue=(alphabetValue+letterValue)-26;
+            position[j]=letterValue; 
+        }else{
+        position[j]=alphabetValue+letterValue;
+        }
     }
     for(let i=0; i<text.length;i++){
-        //helpVariable=position[i];
-        //position[i]=helpVariable-2;
+        //alphabetValue=position[i];
+        //position[i]=alphabetValue-2;
     encrypted+=ALPHABET[position[i]];
     }
     console.log(`Well it seems->${encrypted}`)
     console.log(text);
 }
-let phrase="thereissomethinghere";
-simpleCipher(phrase);
+let phrase="Estoesunmensajetipico";
+let encode="z";
+simpleCipher(phrase,encode);
