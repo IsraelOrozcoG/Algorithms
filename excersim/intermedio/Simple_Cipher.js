525f52c3c18be477d1a1c2c7e089a751cd683973
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-02-27 21:45:46 -0600

    Se valido una parte de Simple Cipher

--- excersim/intermedio/Simple_Cipher.js
+++ excersim/intermedio/Simple_Cipher.js
@@ -2,19 +2,22 @@ const ALPHABET=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 let simpleCipher=(text,letter)=>{
-    text=text.toLowerCase();
-    let encrypted ="";
+    let originalMessage =text;
+    text=text.toLowerCase().replace(/\s/g, "");
+    let letterValid =/[^a-z]/gm;
+    let encrypted ="";   
     let position = [];
     let alphabetValue =0;
-    let letterValue = 0
+    let letterValue = 0;
+    if((letter=="")){
+        return console.log("Empty  letter");
+    }else{
     letterValue=ALPHABET.indexOf(letter.charAt(0));
     
     for(let i=0 ;i<text.length;i++){
-        //console.log(text.charAt(i));
         position.push(ALPHABET.indexOf(text.charAt(i)));
-        //console.log(position);
     }
-    for(let j=0;j<text.length;j++){
+        for(let j=0;j<text.length;j++){
         alphabetValue=position[j];
         if(letterValue+alphabetValue>26){
             letterValue=(alphabetValue+letterValue)-26;
@@ -28,9 +31,9 @@ let simpleCipher=(text,letter)=>{
         //position[i]=alphabetValue-2;
     encrypted+=ALPHABET[position[i]];
     }
-    console.log(`Well it seems->${encrypted}`)
-    console.log(text);
+    console.log(` Your message :${originalMessage}->${encrypted}`)
+} 
 }
-let phrase="Estoesunmensajetipico";
-let encode="z";
+let phrase="This is a typical message";
+let encode="m";
 simpleCipher(phrase,encode);
