a087ff5d90193787f5cd07d80e78d3d61cbac426
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-02-28 20:33:03 -0600

    Se crearon los objetos de las alergias y la funcion Allergies

---
+++ excersim/intermedio/Allergies.js
@@ -0,0 +1,17 @@
+const ALLERGIES ={
+        eggs:1,
+        penauts:2,
+        shellfish:4,
+        strawberries:8,
+        tomatoes:16,
+        chocolate:32,
+        pollen:64,
+        cats:128,
+        shrimp:256,
+        sun:512,
+        gluten:1024
+};
+
+let allergies=()=>{
+
+}
