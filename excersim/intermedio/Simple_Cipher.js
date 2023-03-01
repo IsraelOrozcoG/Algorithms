3116c413a6463409e160099ba7614823e005b8d9
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-02-28 20:25:40 -0600

    Algoritmo simple Cipher no funciona correctamente

--- excersim/intermedio/Simple_Cipher.js
+++ excersim/intermedio/Simple_Cipher.js
@@ -4,7 +4,6 @@ const ALPHABET=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
 let simpleCipher=(text,letter)=>{
     let originalMessage =text;
     text=text.toLowerCase().replace(/\s/g, "");
-    let letterValid =/[^a-z]/gm;
     let encrypted ="";   
     let position = [];
     let alphabetValue =0;
@@ -19,7 +18,7 @@ let simpleCipher=(text,letter)=>{
     }
         for(let j=0;j<text.length;j++){
         alphabetValue=position[j];
-        if(letterValue+alphabetValue>26){
+        if((letterValue+alphabetValue)>26){
             letterValue=(alphabetValue+letterValue)-26;
             position[j]=letterValue; 
         }else{
@@ -27,13 +26,49 @@ let simpleCipher=(text,letter)=>{
         }
     }
     for(let i=0; i<text.length;i++){
-        //alphabetValue=position[i];
-        //position[i]=alphabetValue-2;
     encrypted+=ALPHABET[position[i]];
     }
-    console.log(` Your message :${originalMessage}->${encrypted}`)
+    console.log(` Your message :${originalMessage}->${encrypted}`);
+    return `${encrypted}`;
 } 
 }
+
+let simpleDecipher=(encryptedMessage,letter)=>{
+        let dencrypted ="";   
+        let position = [];
+        let alphabetValue =0;
+        let letterValue = 0;
+
+        letterValue=ALPHABET.indexOf(letter.charAt(0));
+ 
+
+        for(let i=0 ;i<encryptedMessage.length;i++){
+        position.push(ALPHABET.indexOf(encryptedMessage.charAt(i)));
+    }
+
+        for(let j=0;j<encryptedMessage.length;j++){
+        alphabetValue=position[j];
+
+        if(alphabetValue-letterValue<0){
+            letterValue=26-(alphabetValue-letterValue);
+
+            position[j]=letterValue; 
+        }else{
+        position[j]=alphabetValue-letterValue;
+
+        }
+    }
+        for(let i=0; i<encryptedMessage.length;i++){
+        dencrypted+=ALPHABET[position[i]];
+    }
+
+    console.log(dencrypted);
+}
+
+
 let phrase="This is a typical message";
-let encode="m";
-simpleCipher(phrase,encode);
+let encode="b";
+let encryptedMessage="";
+encryptedMessage=simpleCipher(phrase,encode);
+console.log(encryptedMessage);
+simpleDecipher(encryptedMessage,encode);
