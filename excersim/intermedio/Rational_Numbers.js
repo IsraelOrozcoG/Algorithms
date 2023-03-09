b5de8bb7d01088250bf11c176e487f595b2e1709
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-09 13:30:25 -0600

    Se agrego exponenciacion

--- excersim/intermedio/Rational_Numbers.js
+++ excersim/intermedio/Rational_Numbers.js
@@ -1,21 +1,33 @@
 let rationalNumbers=(numerator1,denominator1,numerator2,denominator2)=>{
+
     let resultNumerator=numerator1 * denominator2 + numerator2 * denominator1;
     let resultDenominator=denominator1 * denominator2;
     let addition=`${resultNumerator}/${resultDenominator}`;
+
     resultNumerator=numerator1 * denominator2 - numerator2 * denominator1;
     resultDenominator=denominator1 * denominator2;
     let subtraction=`${resultNumerator}/${resultDenominator}`;
+
     resultNumerator=numerator1*numerator2;
     resultDenominator=denominator1*denominator2;
     let multiplication =`${resultNumerator}/${resultDenominator}`;
+
     resultNumerator=numerator1*denominator2;
     resultDenominator=numerator2*denominator1;
     let divition=`${resultNumerator}/${resultDenominator}`;
+
+    resultNumerator=(numerator1**2);
+    resultDenominator=(denominator1**2);
+    let exponentiation=`${resultNumerator}/${resultDenominator}`;
+    resultNumerator=(numerator2**2);
+    resultDenominator=(denominator2**2);
+    exponentiation+=` ${resultNumerator}/${resultDenominator}`;
     return `The operations of ${numerator1}/${denominator1} , ${numerator2}/${denominator2}
             Addition=${addition}
             Subtraction=${subtraction}
             Multiplication=${multiplication}
-            Divition=${divition}`;
+            Divition=${divition}
+            Exponentiation=${exponentiation}`;
 }
 let n1=10,d1=8,n2=30,d2=24;
 
