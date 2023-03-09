e991e0bbdd177023ee836d54df540a60d00f0abb
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-09 13:18:07 -0600

    Se agrego suma, resta y multiplicacion de numeros racionales

--- excersim/intermedio/Rational_Numbers.js
+++ excersim/intermedio/Rational_Numbers.js
@@ -1,7 +1,17 @@
 let rationalNumbers=(numerator1,denominator1,numerator2,denominator2)=>{
     let resultNumerator=numerator1 * denominator2 + numerator2 * denominator1;
     let resultDenominator=denominator1 * denominator2;
-    return `${resultNumerator}/${resultDenominator}`;
+    let addition=`${resultNumerator}/${resultDenominator}`;
+    resultNumerator=numerator1 * denominator2 - numerator2 * denominator1;
+    resultDenominator=denominator1 * denominator2;
+    let subtraction=`${resultNumerator}/${resultDenominator}`;
+    resultNumerator=numerator1*numerator2;
+    resultDenominator=denominator1*denominator2;
+    let multiplication =`${resultNumerator}/${resultDenominator}`;
+    return `The operations of ${numerator1}/${denominator1} , ${numerator2}/${denominator2}
+            Addition=${addition}
+            Subtraction=${subtraction}
+            Multiplication=${multiplication}`;
 }
 let n1=10,d1=8,n2=30,d2=24;
 
