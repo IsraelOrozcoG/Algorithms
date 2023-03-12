6e5de4fe212fa9a6a5c64e50804ceb2a73a145b5
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-12 14:52:03 -0600

    ECMA2016 a Funcion HAMMING y operador ternario

--- excersim/basico/Hamming.js
+++ excersim/basico/Hamming.js
@@ -1,12 +1,12 @@
-function hamming(strand1,strand2){
+let hamming=(strand1,strand2)=>{
     let distance=0;
+    
     if(strand1.length!=strand2.length){
         return "not the same length";
     }
     for(i=0;i<=strand1.length;i++){
-        if(strand1.charAt(i)!=strand2.charAt(i)){
-            distance ++;
-        }
+        strand1.charAt(i)!=strand2.charAt(i)?distance ++ : "";
+
     }
     return "The Hamming Distance is "+distance;
 }
