25d0aaed366c111c4a605b7b46e676eab5f41177
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-13 16:40:40 -0600

    Cambio de function a ECMA2016

--- excersim/basico/Nucleotide_Count.js
+++ excersim/basico/Nucleotide_Count.js
@@ -1,4 +1,4 @@
-function nucleotideCount(nucleotide){
+let nucleotideCount=(nucleotide)=>{
     let a=0,c=0,g=0,t=0;
     let countedNucleotide=""
     for(i=0;i<=nucleotide.length;i++){
@@ -6,21 +6,6 @@ function nucleotideCount(nucleotide){
         :nucleotide.charAt(i)==="C"?c++
         :nucleotide.charAt(i)==="G"? g++
         :t++;
-        /*
-        if(nucleotide.charAt(i)==="A"){
-            a++;
-        }
-        if(nucleotide.charAt(i)==="C"){
-            c++;
-        }
-        if(nucleotide.charAt(i)==="G"){
-            g++;
-        }
-        if(nucleotide.charAt(i)==="T"){
-            t++;
-        }
-        /*
-        return "Error there is no "+nucleotide.charAt(i)+"check the nucleotide";*/
     }
     return countedNucleotide =nucleotide+"-> A:"+a+" C:"+c+" G:"+g+" T:"+t;
 }
