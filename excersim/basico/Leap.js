bc40f86a506161594b1e97f9e54ce143b1951574
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-13 16:34:54 -0600

    Algoritmo Leap se cambio a ECMA2016 y operador ternario

--- excersim/basico/Leap.js
+++ excersim/basico/Leap.js
@@ -1,13 +1,12 @@
-function Leap(year){
-    if((year%4==0)&&(year%100!=0||year%400==0)){
-        return "The year"+year+" is leap";    
-    }
-        return "The  year"+year+" is not leap";
+let  leap=(year)=>{
+
+    return (year%4==0)&&(year%100!=0||year%400==0)?`The year ${year} is leap`
+            :`The  year ${year} is not leap`;
     }
 
     let y1=1904,y2=1908,y3=1961,y4=400;
 
-    console.log(Leap(y1));
-    console.log(Leap(y2));
-    console.log(Leap(y3));
-    console.log(Leap(y4));
+    console.log(leap(y1));
+    console.log(leap(y2));
+    console.log(leap(y3));
+    console.log(leap(y4));
