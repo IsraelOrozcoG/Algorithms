c7c047818b5373159d0be826f8a8cfbb6c6a5f94
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-13 16:39:16 -0600

    Cambio de if else a operadores ternarios

--- excersim/basico/Nucleotide_Count.js
+++ excersim/basico/Nucleotide_Count.js
@@ -2,7 +2,11 @@ function nucleotideCount(nucleotide){
     let a=0,c=0,g=0,t=0;
     let countedNucleotide=""
     for(i=0;i<=nucleotide.length;i++){
-
+        nucleotide.charAt(i)==="A"?a++
+        :nucleotide.charAt(i)==="C"?c++
+        :nucleotide.charAt(i)==="G"? g++
+        :t++;
+        /*
         if(nucleotide.charAt(i)==="A"){
             a++;
         }
