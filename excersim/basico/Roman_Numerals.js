4abcaca21fe6f09b61515c14aad2c2f147c95632
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-15 17:49:46 -0600

    Algoritmo Roman Numerals funciona correctamente

--- excersim/basico/Roman_Numerals.js
+++ excersim/basico/Roman_Numerals.js
@@ -2,10 +2,10 @@ let romanNumerals=(number)=>{
     
     let romanNumber="";
     let help;
-    let unit=["NAN","I","II","III","IV","V","VI","VII","VIII","IX"];
-    let deceins=["NAN","X","XX","XXX","XL","L","LX,LXX","LXXX","XC"];
-    let centen=["NAN","C","CC","CCC","XD","D","DC,DCC","DCCC","MC"];
-    let miles=["NAN","M","MM","MMM"];
+    let unit=["","I","II","III","IV","V","VI","VII","VIII","IX"];
+    let deceins=["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+    let centen=["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
+    let miles=["","M","MM","MMM"];
     
     if(number.isNaN || number<=0 || number>3999){
         return "Not Posible";
@@ -28,7 +28,7 @@ let romanNumerals=(number)=>{
         romanNumber+=deceins[help];
         number=number%10;
     }
-
+    
     if(number<10){
         help=Math.trunc(number/1);
         romanNumber+=unit[help];
@@ -38,5 +38,8 @@ let romanNumerals=(number)=>{
     return romanNumber;
 }
 
-let myNumber=89;
-console.log(romanNumerals(myNumber));
+let myNumber=3001;
+for(let i=1;i<3001;i++){
+    
+console.log(`${i} = ${romanNumerals(i)}`);
+}
