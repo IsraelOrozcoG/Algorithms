71499d99d0b170421f73c2643b42e384db97b383
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-28 17:04:14 -0600

    Pruebas del Algoritmo diamond

--- excersim/intermedio/diamond.js
+++ excersim/intermedio/diamond.js
@@ -1,6 +1,18 @@
 let diamond = (pattern)=>{
-    return pattern
+    let j=0;
+    let diamond ="";
+
+    for(let i=1;i<pattern;i++){
+        for(let j=1;j<pattern;j++){
+            if(j==pattern-1){
+            diamond+="*";
+            }
+        }
+            diamond+=`l \n`;
+    }
+
+    return diamond
 }
 
-let letter = "a";
+let letter = 7;
 console.log(diamond(letter));
