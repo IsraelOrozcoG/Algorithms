21e2e733c720a4f4beeb4cd7dc604bd2ef04990e
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-29 18:21:56 -0600

    Algoritmo Diamante posiciones y forma de diamanante

--- excersim/intermedio/diamond.js
+++ excersim/intermedio/diamond.js
@@ -1,18 +1,41 @@
 let diamond = (pattern)=>{
-    let j=0;
+    
     let diamond ="";
 
-    for(let i=1;i<pattern;i++){
-        for(let j=1;j<pattern;j++){
-            if(j==pattern-1){
-            diamond+="*";
-            }
+    for (let i = 1; i <= 5; i++) {
+        
+        for (let j = 1; j <= 5 - i; j++) {
+          diamond+='*';
+
         }
-            diamond+=`l \n`;
-    }
+        
+        for (let k = 0; k < 2 * i - 1; k++) {
+            if(k%1+k==0 ){
+                diamond+=i;      
+            }else{
+          diamond+='*';
+        }}
+        for (let j = 1; j <= 5 - i; j++) {
+            diamond+='*';
+  
+          }
+        diamond+=`\n`;
+      }
+
+      for (let i = 1; i <= 5 - 1; i++) {
+        
+        for (let j = 0; j < i; j++) {
+           diamond+=' ';
+        }
+        
+        for (let k = (5 - i) * 2 - 1; k > 0; k--) {
+          diamond+='*';
+        }
+        diamond+=`\n`;
+      }
 
     return diamond
 }
 
-let letter = 7;
+let letter = 10;
 console.log(diamond(letter));
