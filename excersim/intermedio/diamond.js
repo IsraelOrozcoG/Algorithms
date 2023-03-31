be3521fe4aae1657935cf4d655206f99104919cf
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-03-31 17:16:56 -0600

    Algoritmo diamond terminado

--- excersim/intermedio/diamond.js
+++ excersim/intermedio/diamond.js
@@ -1,8 +1,16 @@
+const valuesLetter ={
+    A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8,I:9,J:10,K:11,L:12,M:13,N:14,O:15,P:16,Q:17,R:18,S:19,T:20,U:21,V:22,W:23,X:24,Y:25,Z:26
+}
+
 let diamond = (pattern)=>{
     
     let diamond ="";
     let alphabet =  ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
+    if(pattern.length>1||isNaN(pattern) == false){
+      return `Not possible just 1 letter`
+    }
+    pattern=pattern.toUpperCase();
+    pattern =valuesLetter[pattern];
     for (let i = 1; i <= pattern; i++) {
         
         for (let j = 1; j <= pattern - i; j++) {
@@ -46,5 +54,15 @@ let diamond = (pattern)=>{
     return diamond
 }
 
-let letter = 25;
+let letter = 'V';
+let letter2 = 'c';
+let letter3 = 'rweqeQWE';
+let letter4 = 2356;
+let letter5 = 2;
+let letter6 = `9`;
 console.log(diamond(letter));
+console.log(diamond(letter2));
+console.log(diamond(letter3));
+console.log(diamond(letter4));
+console.log(diamond(letter5));
+console.log(diamond(letter6));
