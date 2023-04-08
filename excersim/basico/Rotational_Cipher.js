47a4793195aea85c7fdfa6ceaeffdeb69bbbc738
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-04-07 22:00:18 -0500

    Se agrego la excepcion fuera del alfabeto

--- excersim/basico/Rotational_Cipher.js
+++ excersim/basico/Rotational_Cipher.js
@@ -2,6 +2,9 @@ function rotationalCipher(rot){
     let alphabet="abcdefghijklmnopqrstuvwxyz"
     let cipher="";
     let i=0;
+    if(rot==26){
+        return `Outside the alphabet`;
+    }
     console.log(alphabet.charAt(rot));
     do{
         
@@ -11,5 +14,5 @@ function rotationalCipher(rot){
     }while(i!=26)
 }
 
-let rot=10;
-rotationalCipher(rot);
+let rot=26;
+console.log(rotationalCipher(rot));
