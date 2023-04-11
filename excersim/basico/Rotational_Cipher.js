2f45f1e7c4500ad1f915a44f541362ab324006ea
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-04-10 20:46:08 -0500

    Algoritmo Rotational Cipher terminado

--- excersim/basico/Rotational_Cipher.js
+++ excersim/basico/Rotational_Cipher.js
@@ -1,17 +1,37 @@
-function rotationalCipher(rot){
+function rotationalCipher(rot,phrase){
     let alphabet="abcdefghijklmnopqrstuvwxyz"
     let isAlphabet=/[a-z]/i
     let placeOfLetter= 0;
+    let encrypth = ``;
     rot=rot.toLowerCase();
+    /* Verify valid input */
     if(isAlphabet.test(rot)== false || rot.length>1){
         return -1;
     } 
+    /*Obtain the value */
     while( rot != alphabet.charAt(placeOfLetter) ){
         placeOfLetter++;
     }
-    return placeOfLetter;
+
+for(let j = 0;j<phrase.length;j++ )
+{
+for(let i=0;i<alphabet.length;i++)
+{
+        if(phrase.charAt(j)==alphabet.charAt(i))
+        {
+            if(i+placeOfLetter > 25){
+
+                encrypth +=alphabet.charAt((i+placeOfLetter)-25-1);
+            }else{
+            encrypth +=alphabet.charAt(i+placeOfLetter);
+            }
+        }
+
+    }}
+    return  `Phrase ${phrase} , encription ${rot} => ${encrypth}`;
 
 }
 
-let rot=`h`;
-console.log(rotationalCipher(rot));
+let rot=`z`;
+let phrase = `malaquite`;
+console.log(rotationalCipher(rot,phrase));
