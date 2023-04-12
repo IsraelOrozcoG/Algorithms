064060cb16192380e538976de58258a2d0a5276f
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-04-11 22:53:05 -0500

    Validacion de tamanio y transformacion de numero ISBN

--- excersim/intermedio/ISBN_Verifier.js
+++ excersim/intermedio/ISBN_Verifier.js
@@ -1,5 +1,9 @@
 let isbnVerifier =(isbn) =>{
- console.log(isbn);
+    isbn=isbn.replace(/\-/g, "");
+    if(isbn.length!=10){
+        return console.log("No valid ISBN");
+    }
+    console.log(isbn);
 }
 
 let isbn = "3-598-21508-8";
