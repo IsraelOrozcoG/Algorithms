00f7898cb3c847642a768d395da75fe25febf83c
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-04-11 23:22:14 -0500

    Algoritmo ISBN Verifier terminado

--- excersim/intermedio/ISBN_Verifier.js
+++ excersim/intermedio/ISBN_Verifier.js
@@ -1,11 +1,24 @@
 let isbnVerifier =(isbn) =>{
+    let isbnString = isbn ;
     isbn=isbn.replace(/\-/g, "");
+    let flag = 0;
+    let toNumber = 0 , multiplier = 10;
+    let calculate = 0;
     if(isbn.length!=10){
         return console.log("No valid ISBN");
     }
-    console.log(isbn);
+    while( flag < 10 ){
+        /*Other way to cast string to number  = + */
+        toNumber= + isbn.charAt(flag);
+        calculate += toNumber * multiplier;
+        
+        multiplier --;
+        flag ++;
+    }
+     
+    return calculate % 11 == 0 ? `The ISB ${isbnString} is valid`: `The ISB ${isbnString} is not valid`
 }
 
 let isbn = "3-598-21508-8";
 
-isbnVerifier(isbn);
+console.log(isbnVerifier(isbn));
