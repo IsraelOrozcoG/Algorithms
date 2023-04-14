5be336052eca7ac80656c7a0f6f83650b9eea8e6
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-04-13 20:45:03 -0500

    Primera parte de algoritmo Largest Product Series

--- excersim/intermedio/Largest_Product_Series.js
+++ excersim/intermedio/Largest_Product_Series.js
@@ -2,22 +2,31 @@ let largestSeriesProduct = (series,span) =>{
     let size = number.length;
     let flag  = 0;
     let concat = ``;
+    let collection= [];
+    let aux = ``;
 
-    for(let i = 0 ; i< size ; i+=span){
+    for(let i = 0 ; i< size*span; i+=span){
         for(let j = 0; j < span; j++){
             concat += series.charAt(flag);
             flag ++;
             
         }
+            
+            if(concat.length == span){
+                collection.push (concat);
+            }
             concat += `\n`;
-            concat += series.charAt(flag-1);
+            flag -=(span-1);
+            concat = ``;
+            //concat += series.charAt(i);
+            
             
     }
 
-    return `${concat}`;
-}
+    return `Coleccion ${collection}`;
+} 
 
 let quantity = 2;
-let number ="987654";
+let number ="987654321";
 
 console.log(largestSeriesProduct(number,quantity));
