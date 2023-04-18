9b6dd2e411548adf288c272d7925d9728013808c
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-04-17 22:42:21 -0500

    Creando nuevo algoritmo

---
+++ excersim/intermedio/Strain.js
@@ -0,0 +1,8 @@
+let strain = (numbers, predicate) =>{
+    return numbers, predicate;
+}
+
+let myNumbers = [1,2,3,4,5,6,7,8,9];
+let predicate = "even";
+
+console.log(strain(myNumbers,predicate));
