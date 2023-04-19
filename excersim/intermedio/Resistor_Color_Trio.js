fa5ccc93221513b1957bf94249183b7e90c58437
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-04-19 17:57:32 -0500

    Algoritmo Resistor Color Trio muestra el valor, ahora faltan los valores

--- excersim/intermedio/Resistor_Color_Trio.js
+++ excersim/intermedio/Resistor_Color_Trio.js
@@ -12,11 +12,21 @@ const RESISTOR_COLOR_VALUE = {
 }
 
 let resistorColorTrio = (c1,c2,c3) =>{
-    console.log(`${c1} ${c2} ${c3}`);
+    let firstValue = 0, secondValue =0, thirdValue = 0;
+    let zeros = ``;
+    firstValue = RESISTOR_COLOR_VALUE[c1];
+    secondValue = RESISTOR_COLOR_VALUE[c2];
+    thirdValue = RESISTOR_COLOR_VALUE[c3];
+    
+    for(let i = 0; i< thirdValue ;i++){
+        zeros += `0`;
+    }
+
+    console.log(`${firstValue}${secondValue}${zeros} ohms`);
 } 
 
 let firstColor =`red`;
-let secondColor = `black`;
+let secondColor = `violet`;
 let thirdColor = `orange`;
 
 resistorColorTrio (firstColor,secondColor,thirdColor);
