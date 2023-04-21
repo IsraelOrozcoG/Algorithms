32ea64f004441588a620e8f6f5adb8de9dacfeb7
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2023-04-20 20:44:10 -0500

    Algoritmo para List Operations inicializado

---
+++ excersim/intermedio/List_Operations.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(element) {
+        this.element = element;
+        this.next = null;
+    }
+}
+class List {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    appendList (){
+
+    }
+
+    concatenateList () {
+
+    }
+
+    filterList () {
+
+    }
+
+    lengthList () {
+
+    }
+
+    mapList () {
+
+    }
+
+    foldrList () {
+
+    }
+
+    foldlList () {
+
+    }
+
+    reverseList (){
+        
+    }
+
+}
